e1c8290086e04179a472b57a71d2a1969e61a479
Izhari Ishak Aksa <izharishaksa@gmail.com>
2021-12-04 13:45:53 +0700

    Update str.go

--- internal/xxx/str.go
+++ internal/xxx/str.go
@@ -2,7 +2,7 @@ package xxx
 
 func Reverse(s string) string {
 	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/3; i, j = i+1, j-1 {
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
